3acebb0fe6b39a59d39089b72d7e5eadc173bc21
goutkannan <gout.kannan@gmail.com>
2016-06-30 03:25:03 -0500

    config with credentials

--- SwingApp/src/eStore/MakeConnection.java
+++ SwingApp/src/eStore/MakeConnection.java
@@ -1,16 +1,45 @@
 package eStore;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Paths;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class MakeConnection {
+	public static String user;
+	public static String pwd;
+		@SuppressWarnings("unused")
+		public static void Connection() throws IOException {
+			
+				Properties prop = new Properties();
+				String propFileName = Paths.get(".").toAbsolutePath().normalize().toString()+"\\config.properties";
+				
+	 
+				InputStream  inputStream=  new FileInputStream("config.properties"); 
+	 
+				if (inputStream != null) {
+					prop.load(inputStream);
+				} 
+				else {
+					throw new FileNotFoundException("property file '" + propFileName + "' not found in the classpath");
+				}
+				user = prop.getProperty("user");
+				pwd = prop.getProperty("password");
+		
+	}
+		
+		
         public static Connection getDafaultConnection() {
                 try {
-
+                		Connection();
                         Class.forName("com.mysql.jdbc.Driver").newInstance();
                         Connection conn = null;
 
-                        conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/", "root", "goutham911");
+                        conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/", user, pwd);
                         System.out.println("\n Driver is connected Successfully");
 
                         return conn;
@@ -28,11 +57,11 @@ public class MakeConnection {
         }
         public static Connection getConnection(String databaseName) {
                 try {
-
+                		Connection();
                         Class.forName("com.mysql.jdbc.Driver").newInstance();
                         Connection conn = null;
 
-                        conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/"+databaseName, "root", "goutham911");
+                        conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/"+databaseName, user, pwd);
                         System.out.println("\n Driver is connected Successfully");
 
                         return conn;
